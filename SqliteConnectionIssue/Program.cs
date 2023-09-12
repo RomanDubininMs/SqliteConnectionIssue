8b5ddf4be92a98ec781e3c01b0e7652d079605ea
Roman Dubinin <romandubinin@microsoft.com>
2023-09-12 14:18:02 +0200

    Sample project showing that db file is not released after connection to db is closed

---
+++ SqliteConnectionIssue/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var dbFilePath = "xxx.db";
+
+        await DoSomeDbWorkAsync(dbFilePath, cancellationToken).ConfigureAwait(false);
+
+        File.Delete(dbFilePath);
+    }
+
+    private static async Task DoSomeDbWorkAsync(string dbFilePath, CancellationToken cancellationToken)
+    {
+        await using var connection = new SqliteConnection($"Data Source={dbFilePath}");
+        await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+    }
+}
